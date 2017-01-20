1da409f5ec28abf4774aafde1f66cfb71f8257bf
kelian33 <kelian.gardin@ynov.com>
2017-01-20 16:24:05 +0100

    page connexion

---
+++ Etape3/Olibrary/connexion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connexion</title>
+</head>
+<body>
+<form method="post">
+	<input type="text" name="mail" placeholder="Adresse Mail">
+	<input type="password" name="password" placeholder="Mot de passe">
+	<input type="submit" name="connexion" value="Se connecter">
+</form>
+</body>
+</html>
