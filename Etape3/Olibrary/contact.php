3ecd4a23118d2164482ec6800751b616f605f46c
kelian33 <kelian.gardin@ynov.com>
2017-01-20 16:19:14 +0100

    modif page contact

--- Etape3/Olibrary/contact.php
+++ Etape3/Olibrary/contact.php
@@ -3,39 +3,30 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>page contacte</title>
+    <title>Contact</title>
 </head>
 <body>
 
-<form action="/ma-page-de-traitement" method="post">
-    <div>
-        <label for="nom">*Nom :</label>
-        <input type="text" id="nom" />
-    </div>
-    <div>
-        <label for="prenom">*Prenom :</label>
-        <input type="text" id="prenom" />
-    </div>
-    <div>
-        <label for="courriel">*Courriel :</label>
-        <input type="email" id="courriel" />
-    </div>
-    <div>
+    <form action="" method="post">
+
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" required="" /> <br>
+
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" required="" /> <br>
+
+        <label for="courriel">Adresse mail :</label>
+        <input type="email" id="courriel" name="mail" required="" /> <br>
+
         <label for="telephone">Téléphone :</label>
-        <input type="text" id="telephone" />
-    </div>
-    <div>
+        <input type="text" id="telephone" name="phone" /> <br>
+
         <label for="message">Message :</label>
-        <textarea id="message"></textarea>
-    </div>
-    <div class="button">
+        <textarea id="message"></textarea> <br>
+
         <button type="submit">Envoyer votre message</button>
-    </div>
-<p>(*) Champ obligatoire </p>
-</form>
 
-
-
+    </form>
 
 </body>
 </html>
