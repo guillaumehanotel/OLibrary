9e06c0d9ec54ec57e80461f2a3424d0c952f3831
guillaumehanotel <guiguihanotel@orange.fr>
2017-01-27 19:29:00 +0100

    Architecture MVC

---
+++ Etape3/Olibrary/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$_dir["controllers"] = "controllers/";
+$_dir["views"] = "/views/";
+
+$config = array(
+	"loginBDD" => "olibrary",
+	"mdpBDD" => "erty",
+	"base" => "bdd_olibrary",
+	);
+
+	
---
+++ Etape3/Olibrary/controllers/404.Controller.php
@@ -0,0 +1,2 @@
+<?php
+require $_dir["views"]."404.php";
