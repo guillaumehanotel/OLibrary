28a3c6b584bb146464aad8ae61f5565b6de6ea2e
guillaumehanotel <guiguihanotel@orange.fr>
2017-01-29 18:45:49 +0100

    organisation CSS

---
+++ Etape3/Olibrary/views/Accueil.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link href="../css/style.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
+        <title>OLibrary</title>
+
+    </head>
+    <body>
+
+
+
+
+        <header>
+
+            <div id="flou"></div>
+
+            <div id="top">
+
+                <div id="logo">
+                    <img src="../img/logo3.png"  />
+                    <h1>OLibrary</h1>
+                </div>
+
+
+                <div id="logs">
+                    <a href="#">Connexion</a>
+                    <a href="#">Inscription</a>
+                </div>
+
+            </div>
+
+
+
+        </header>
+
+
+
+        <!-- -->
+
+
+        <main>
+
+
+        </main>
+
+
+
+
+
+
+
+
+        <footer>
+
+
+        </footer>
+
+
+    </body>
+</html>
