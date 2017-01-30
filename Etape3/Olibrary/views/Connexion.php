8595c0aa2f544d9cc22052f4b5665290a4ad2a16
hugomonfouga <hugo.monfouga@ynov.com>
2017-01-30 09:05:56 +0100

    Up de la page connexion

--- Etape3/Olibrary/views/Connexion.php
+++ Etape3/Olibrary/views/Connexion.php
@@ -1,5 +1,13 @@
 <main id="connexion">
 
+    <form style="text-align: center;" method="POST">
+        Adresse Mail : <br>
+        <input name="username" required="" type="mail"><br>
+        Mot de passe : <br>
+        <input name="password" type="password"> <br>
+        <br>
+        <input type="submit" value="Se connecter">
 
+    </form>
 
 </main>
