c68df4cb26fd6f8d3e8b93b002d2cb91179a0970
cyriaque16 <cyriaque.pitault@ynov.com>
2017-02-03 18:03:10 +0100

    page contact

--- Etape3/Olibrary/views/Contact.php
+++ Etape3/Olibrary/views/Contact.php
@@ -1,23 +1,18 @@
 
-<main id="contact">
-    <form action="" method="post">
+<main class="container" id="contact">
+    <div class="login">
+        <div class="login-triangle"></div>
 
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" required="" /> <br>
+        <h2 class="login-header">Inscription</h2>
 
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" required="" /> <br>
-
-        <label for="courriel">Adresse mail :</label>
-        <input type="email" id="courriel" name="mail" required="" /> <br>
-
-        <label for="telephone">Téléphone :</label>
-        <input type="text" id="telephone" name="phone" /> <br>
-
-        <label for="message">Message :</label>
-        <textarea id="message"></textarea> <br>
-
-        <button type="submit">Envoyer votre message</button>
-
-    </form>
+        <form class="login-container">
+            <p><input type="text" placeholder="Nom"></p>
+            <p><input type="text" placeholder="Prénom"></p>
+            <p><input type="email" placeholder="Email"></p>
+            <p><input type="password" placeholder="Password"></p>
+            <p><input type="text" placeholder="N° Téléphone"></p>
+            <p><input type="text" placeholder="Adresse"></p>
+            <p><input type="submit" value="Se connecter"></p>
+        </form>
+    </div>
 </main>
