79c4f8699c1122662c3dd098acfac10084c828ee
kelian33 <kelian.gardin@ynov.com>
2017-02-03 17:50:54 +0100

    php page inscription

--- Etape3/Olibrary/views/Inscription.php
+++ Etape3/Olibrary/views/Inscription.php
@@ -1,14 +1,13 @@
 <main id="inscription">
 
 	<div class="background">
-	<form method="post">
+	<form method="post" action="">
 		<input type="text" name="nom" placeholder="Nom"><br>
 		<input type="text" name="prenom" placeholder="Prénom"><br>
 		<input type="email" name="email" placeholder="Adresse mail"><br>
 		<input type="password" name="password" placeholder="Mot de passe"><br>
 		<input type="password" name="repassword" placeholder="Confirmer le mot de passe"><br>
 		<input type="submit" name="Valider">
-		<input type="reset" name="Reinitialiser">
 	</form>
 	</div>
 
