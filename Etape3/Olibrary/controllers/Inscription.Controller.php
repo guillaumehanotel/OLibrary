c55ec38cc2637405eef87c41df7c22de56fa7e8d
kelian33 <kelian.gardin@ynov.com>
2017-02-03 15:27:40 +0100

    inscritption php debut

--- Etape3/Olibrary/controllers/Inscription.Controller.php
+++ Etape3/Olibrary/controllers/Inscription.Controller.php
@@ -1,3 +1,20 @@
 <?php
 require $_dir["views"]."Inscription.php";
 
+if(!empty($_POST)){
+    if(!empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["mail"]) && !empty($_POST["password"]) && !empty($_POST["repassword"]) &&
+        isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["repassword"])) {
+            $nom = $_POST["nom"];
+            $prenom = $_POST["prenom"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $repassword = $_POST["repassword"];
+
+
+
+            echo "okay";
+    }
+    else {
+        echo "Merci de remplir tout les champs.";
+    }
+}
