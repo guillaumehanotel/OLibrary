a34896165510b0be1c8e460ba100f1453f408bbf
kelian33 <kelian.gardin@ynov.com>
2017-02-05 19:14:14 +0100

    php page gestion autorité

--- Etape3/Olibrary/views/GestionAutorites.php
+++ Etape3/Olibrary/views/GestionAutorites.php
@@ -1,7 +1,70 @@
 <main id="gestionAutorites"  class="container">
+<h2>Gestion des Autorités</h2>
+
+    <h3>Ajouter un auteur :</h3>
+    <form action="" method="post">
+        <input type="text" name="auteur_nom" placeholder="Nom">
+        <input type="text" name="auteur_prenom" placeholder="Prénom">
+        <input type="submit" name="auteur">
+    </form>
+
+    <h3>Ajouter un editeur :</h3>
+    <form action="" method="post">
+        <input type="text" name="editeur_nom" placeholder="Nom de l'éditeur">
+        <input type="submit" name="editeur">
+    </form>
+
+    <h3>Ajouter un fournisseur :</h3>
+    <form action="" method="post">
+        <input type="text" name="fournisseur_nom" placeholder="Nom du fournisseur">
+        <input type="submit" name="fournisseur">
+    </form>
+
+    <h3>Ajouter une collection :</h3>
+    <form action="" method="post">
+        <input type="text" name="collection_nom" placeholder="Nom de la collection">
+        <select name="editeur_id">
+            <?php
+                    foreach($editeur_id as $edit_id){
+                    echo "<option>".$edit_id['editeur_id'];?> </option><?php } ?>
+        </select>
+        <input type="submit" name="collection">
+    </form>
+
+    <h3>Ajouter un livre :</h3>
+    <form action="" method="post">
+        <input type="text" name="livre_ISBN" placeholder="ISBN">
+        <input type="text" name="livre_titre" placeholder="Titre">
+        <input type="text" name="nb_exemplaire" placeholder="Nombre d'exemplaires">
+        <input type="date" name="date" placeholder="Date parution">
+
+        <textarea name="synopsis" placeholder="Synopsis"></textarea>
+
+        <select name="auteur_id">
+            <option disabled selected>Auteur id </option>
+            <?php
+            foreach($auteur_id as $aut_id){
+                echo "<option>".$aut_id['auteur_id'];?> </option><?php } ?>
+        </select>
+
+        <select name="collection_id">
+            <option disabled selected>Collection id </option>
+            <?php
+            foreach($collection_id as $coll_id){
+                echo "<option placeholder='Collection'>".$coll_id['collection_id'];?> </option><?php } ?>
+        </select>
+
+        <select name="fournisseur_id">
+            <option disabled selected>Fournisseur id </option>
+            <?php
+            foreach($fournisseur_id as $fourn_id){
+                echo "<option>".$fourn_id['fournisseur_id'];?> </option><?php } ?>
+        </select>
+
+        <input type="submit" name="livre">
 
 
-
+    </form>
 
 
 
