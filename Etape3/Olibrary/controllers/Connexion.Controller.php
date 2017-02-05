aa7f2a321d96012c7172c16c6c1dd94e93cec6dc
kelian33 <kelian.gardin@ynov.com>
2017-02-05 12:55:18 +0100

    php page connexion suite2

--- Etape3/Olibrary/controllers/Connexion.Controller.php
+++ Etape3/Olibrary/controllers/Connexion.Controller.php
@@ -1,5 +1,5 @@
 <?php
-require $_dir["views"]."Connexion.php";
+/*require $_dir["views"]."Connexion.php";
 
 if(!empty($_POST)){
     if(!empty($_POST["email"]) && !empty($_POST["password"]) &&
@@ -27,6 +27,55 @@ if(!empty($_POST)){
         if(!empty($_SESSION["erreur"])){
             echo "<div>".$_SESSION['erreur']."</div>";
             unset($_SESSION["erreur"]);
+        }
+
+
+    }
+
+    else{
+        echo "Merci de remplir tout les champs";
+    }
+}*/
+
+
+require $_dir["views"]."Connexion.php";
+
+if(!empty($_POST)){
+    if(!empty($_POST["email"]) && !empty($_POST["password"]) &&
+        isset($_POST["email"]) && isset($_POST["password"])){
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $requete = "SELECT user_mail, user_mdp FROM utilisateur WHERE user_mail='$email'";
+        $req = $bdd->query($requete);
+
+        $user = $req->fetch(PDO::FETCH_OBJ);
+       /* var_dump($requete);
+        var_dump($user);
+        var_dump($user['user_mdp']);
+        var_dump($password);*/
+
+        $passhach = '$2y$10$ies5lZIQFf6UDnlcjR3Jk.7BzPrCQeT00zlSuEonrTmFoSaL1Sv4y';
+
+
+
+        if(password_verify($password,/*$user['user_mdp']*/$passhach)) {
+            $_SESSION["connect"]=true;
+            $_SESSION["user"]=$user['user_nom'];
+            $_SESSION["admin"]=$user['is_admin'];
+            echo "OKAYYYYYYYY";
+            var_dump($user);
+        }
+
+
+        else {
+            $_SESSION["erreur"]="Identification impossible. Verifiez vos identifiants";
+        }
+
+        if(!empty($_SESSION["erreur"])){
+            echo "<div>".$_SESSION['erreur']."</div>";
+            unset($_SESSION["erreur"]);
+            var_dump(password_verify($_POST['password'], $user['user_mdp']));
         }
 
 
