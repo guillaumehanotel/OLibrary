a865b156d8a253bfb28685e24b147b4bdcf1ac25
guillaumehanotel <guiguihanotel@orange.fr>
2017-02-10 03:22:40 +0100

    Revert "tentative BootStrap + Modification Htaccess et pas connexion avec BootStrap"
    This reverts commit fcd22bace73d70944aa6862742f5a3fdbabcdcb7.

--- Etape3/Olibrary/views/Connexion.php
+++ Etape3/Olibrary/views/Connexion.php
@@ -1,16 +1,13 @@
 <main id="connexion" class="container">
 
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
+    <form style="text-align: center;" method="POST">
+        Adresse Mail : <br>
+        <input name="email" required="" type="mail"><br>
+        Mot de passe : <br>
+        <input name="password" type="password"> <br>
+        <br>
+        <input type="submit" value="Se connecter">
 
-
-        <button type="submit" class="btn btn-default">Submit</button>
     </form>
+
 </main>
--- Etape3/Olibrary/views/MenuGestion.php
+++ Etape3/Olibrary/views/MenuGestion.php
@@ -1,22 +1,5 @@
-<main id="menuGestion">
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-3"
-             <h1>Gestion des autorités</h1>
-    </div>
-
-    <div class="row">
-        <div class="col-md-6"
-        <button type="button">Gestion des Notices et des Exemplaires</button>
-        <button type="button">Gestion des Utilisateurs</button>
-        <button type="button">Gestion des Autorités</button>
-        <button type="button">Gestion des emprunteurs</button>
-        <button type="button">Circulation des Exemplaires</button>
-
-    </div>
+<main id="menuGestion"  class="container">
 
 
-</div>
 
 </main>
