9adb6aaef1523c66bbdf20c4a93092eb96acd9b7
hugomonfouga <hugo.monfouga@ynov.com>
2017-02-10 16:18:50 +0100

    Page gestion qui work

--- Etape3/Olibrary/views/MenuGestion.php
+++ Etape3/Olibrary/views/MenuGestion.php
@@ -1,21 +1,37 @@
-<main id="menuGestion"  class="content container">
+<main id="menuGestion" class="content container" xmlns="http://www.w3.org/1999/html">
 
            <h2 class="soustitre">Gestionnaire de l'application</h2>
-           
-           <div class="row">
-               
-               <!-- à toi de jouer ! --> 
-               
-               
-               <Button href="<?php echo BASE_URL."/Olibrary/notices/"; ?>">Gestion des notices et des exemplaires</Button>
-               <Button href="<?php echo BASE_URL."/Olibrary/utilisateurs/"; ?>">Gestion des  Utilisateurs</Button>
-               <Button href="<?php echo BASE_URL."/Olibrary/autorites/"; ?>">Gestion des Autorités</Button>
-               <br>  <br> <br>
-               <Button style="width: 300px" href="<?php echo BASE_URL."/Olibrary/emprunteurs/"; ?>">Gestion des  Emprunteurs</Button>
-               <Button style="width: 300px" href="<?php echo BASE_URL."/Olibrary/documents/"; ?>">Gestion  des Documents</Button>
+
+    <div class="row">
 
 
-               
-           </div>
+
+        <!-- à toi de jouer ! -->
+
+
+        <div class="col-xs-1"></div>
+
+        <div class="col-xs-2">
+        <a  href="<?php echo BASE_URL."/Olibrary/notices/"; ?>">Gestion des notices et des exemplaires</a>
+        </div>
+        <div class="col-xs-2">
+        <a href="<?php echo BASE_URL."/Olibrary/utilisateurs/"; ?>">Gestion des  Utilisateurs</a>
+        </div>
+        <div class="col-xs-2">
+        <a href="<?php echo BASE_URL."/Olibrary/autorites/"; ?>">Gestion des Autorités</a>
+        </div>
+        <div class="col-xs-2">
+        <a href="<?php echo BASE_URL."/Olibrary/emprunteurs/"; ?>">Gestion des  Emprunteurs</a>
+        </div>
+        <div class="col-xs-2">
+        <a href="<?php echo BASE_URL."/Olibrary/documents/"; ?>">Gestion  des Documents</a>
+        </div>
+
+
+
+
+
+
+    </div>
 
 </main>
