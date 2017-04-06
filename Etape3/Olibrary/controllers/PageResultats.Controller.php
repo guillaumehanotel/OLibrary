03a11fd5363f8716ccbfaf5c22b2fc6f5b38922d
guillaumehanotel <guiguihanotel@orange.fr>
2017-04-06 11:43:20 +0200

    Gestion des autorités + début recherche

--- Etape3/Olibrary/controllers/PageResultats.Controller.php
+++ Etape3/Olibrary/controllers/PageResultats.Controller.php
@@ -6,48 +6,6 @@ $requetelivre=$bdd->query("SELECT * FROM livre l JOIN auteur a ON l.auteur_id = 
 $requete_livre = $requetelivre->fetchAll();
 
 
-if($requete_livre==false){
-    ?>
-
-    <div class='container'>
-        <br>
-        <nav id="searchbar" class="col m12 valign">
-            <div class="nav-wrapper">
-                <form action="<?php echo BASE_URL."/pageresultats/"; ?>" method="get">
-                    <div class="input-field" id="inputsearch">
-                        <input id="search" type="search" name="recherche" required>
-                        <label class="label-icon" for="search"><i id="loupe" class="material-icons">search</i></label>
-                        <i class="material-icons">close</i>
-                    </div>
-                </form>
-            </div>
-        </nav>
-        <h3>Aucun résultat</h3>
-    </div>
-
-<?php
-
-}
-elseif($requete_livre==true){
-    ?>
-    <div class='container'>
-        <br>
-        <nav id="searchbar" class="col m12 valign">
-            <div class="nav-wrapper">
-                <form action="<?php echo BASE_URL."/pageresultats/"; ?>" method="get">
-                    <div class="input-field" id="inputsearch">
-                        <input id="search" type="search" name="recherche" required>
-                        <label class="label-icon" for="search"><i id="loupe" class="material-icons">search</i></label>
-                        <i class="material-icons">close</i>
-                    </div>
-                </form>
-            </div>
-        </nav>
-        <h2>Résultats de la recherche</h2></div>
-
-<?php
-}
-
 
 
 require $_dir["views"]."PageResultats.php";
