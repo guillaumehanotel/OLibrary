425b64f26954894c00434b4bfd79323ef3e4f884
guillaumehanotel <guiguihanotel@orange.fr>
2017-04-24 14:25:51 +0200

    corr

--- Etape3/Olibrary/views/GestionNotices.php
+++ Etape3/Olibrary/views/GestionNotices.php
@@ -10,22 +10,25 @@
                 <th data-field="titre"><a id="titre" href="#" class="asc">Titre</a></th>
                 <th data-field="auteur"><a id="auteur" href="#" class="asc">Auteur</a></th>
                 <th data-field="nb">Nombre d'exemplaires</th>
-                <th data-field="showExempl">Voir les exemplaires</th>
+               <!-- <th data-field="showExempl">Voir les exemplaires</th> -->
             </tr>
             </thead>
             <tbody id="bodyLivres">
 
 
 
-            <!-- -->
+
             <?php
             foreach ($resultat_livre as $value){?>
-                <tr>
-                    <td class="notice_titre"><?= $value['notice_titre'];?></td>
-                    <td class="auteur_name"><?= $value['auteur_prenom'];?> <?=$value['auteur_nom'];?></td>
-                    <td class="cpt"><?= $value['cpt'];?></td>
-                    <td class="link"> <a href='<?= BASE_URL."/exemplaires"; ?>/?id=<?= $value['notice_id'] ?>'><i class="small material-icons">visibility</i></a></td>
-                </tr>
+
+                    <tr class="row_notice_<?= $value['notice_id'] ?>">
+                        <td class="notice_titre"><?= $value['notice_titre'];?></td>
+                        <td class="auteur_name"><?= $value['auteur_prenom'];?> <?=$value['auteur_nom'];?></td>
+                        <td class="cpt"><?= $value['cpt'];?></td>
+
+                    </tr>
+
+
                 <?php
             }
             ?>
