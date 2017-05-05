80156b702bf0ff8fb1d625ed64580be1e9d89068
hugomonfouga <hugo.monfouga@ynov.com>
2017-05-05 16:15:03 +0200

    Supppression User

--- Etape3/Olibrary/views/GestionUtilisateurs.php
+++ Etape3/Olibrary/views/GestionUtilisateurs.php
@@ -4,26 +4,24 @@
            <h2 class="soustitre">Gestion des utilisateurs</h2>
 
 
-    <table>
+    <table class="centered striped">
         <thead>
         <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>mail</th>
-            <th>Admin</th>
-            <th>Supprimer</th>
+
+            <th class="user_prename">Nom - Prénom</th>
+            <th class="user_mail">mail</th>
+            <th class="user_delete">Supprimer</th>
         </tr>
         </thead>
          <tr>
             <?php foreach ($resultat_utilisateur as $utilisateur)
             { ?>
-                <td><?= $utilisateur['user_num']; ?></td>
-               <td><?= $utilisateur['user_nom']; ?></td>
-               <td><?= $utilisateur['user_prenom']; ?></td>
+
+               <td><?php if($utilisateur['is_admin']==true){
+                    echo "<i id='admin' class='material-icons'>perm_identity</i>";
+                } ?><?= $utilisateur['user_nom']; ?> <?= $utilisateur['user_prenom']; ?></td>
             <td> <?= $utilisateur['user_mail']; ?> </td>
-            <td> <?= $utilisateur['is_admin']; ?></td>
-            <td> <?= '<a href="supprimer.php?id='.$utilisateur['user_num'].'">Supprimer</a>'; ?> </td>
+            <td>  <a href="<?= BASE_URL."/supprUser";?>/?id=<?= $utilisateur['user_num']?>"> <i class='material-icons'>delete</i></a> </td>
         </tr>
         <?php
         } 
--- Etape3/Olibrary/views/supprimer.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = $_POST['id'];
-if (isset($id) AND !(empty($id)))
-{
-    connect_db ('localhost', 'olibrary', 'erty', 'olibrary');
-    $req = $bdd->query("DELETE FROM utilisateur WHERE id=$id");
-    HEADER('Loacation : GestionUtilisateurs.php');
-}
